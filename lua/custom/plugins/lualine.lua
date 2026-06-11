4c9041584b2b47245678bc67f3f80bf111ac089f
Kamil Sasin <k.sasin@trendenergysolutions.pl>
2026-06-11 15:06:50 +0200

    add lualine :D

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,20 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      theme = 'catppuccin-nvim',
+      section_separators = { left = '', right = '' },
+      component_separators = '',
+      icons_enabled = true,
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { 'filename' },
+      lualine_x = { 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
+    },
+  },
+}
