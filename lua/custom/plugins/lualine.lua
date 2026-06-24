978e577dc0e20c77b210d5e001480e24aacd4738
Kamil Sasin <k.sasin@trendenergysolutions.pl>
2026-06-24 13:44:39 +0200

    change lualine style

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -10,7 +10,22 @@ return {
     },
     sections = {
       lualine_a = { 'mode' },
-      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_b = {
+        {
+          'branch',
+          fmt = function(name)
+            -- grab the first "word" (leading run of letters/digits);
+            -- stops at the first separator: - _ / . etc.
+            local first = name:match '^%w+'
+            if first and #first < #name then
+              return first .. '…'
+            end
+            return name
+          end,
+        },
+        'diff',
+        'diagnostics',
+      },
       lualine_c = { 'filename' },
       lualine_x = { 'filetype' },
       lualine_y = { 'progress' },
